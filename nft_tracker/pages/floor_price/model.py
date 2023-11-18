3552fd126607973887a090f056c2084168066025
Vikas Negi <vikas.negi10@gmail.com>
2023-11-18 16:49:57 +0100

    Add logic for markets plot

--- nft_tracker/pages/floor_price/model.py
+++ nft_tracker/pages/floor_price/model.py
@@ -1,5 +1,6 @@
 import plotly.graph_objects as go
-from utils.data import get_nft_hist
+from utils.data import get_nft_hist, get_nft_markets
+import plotly.express as px
 import pandas as pd
 pd.options.plotting.backend = "plotly"
 
@@ -9,5 +10,14 @@ def plot_nft_hist(nft_id, num_days):
                        template = "seaborn",
                        labels = dict(index = "Time", value = "Price [USD]"),
                        markers = True)
+    fig = go.Figure(fig)
+    return fig
+
+def plot_nft_markets(num_entries, order_by):
+    df_nft_markets = get_nft_markets(num_entries, order_by)
+    fig = px.bar(df_nft_markets,
+                 x = "id",
+                 y = "floor_price_usd",
+                 color = "asset_platform_id")
     fig = go.Figure(fig)
     return fig
