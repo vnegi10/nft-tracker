69182425ff6ded6a986bca1237917da691be92a2
Vikas Negi <vikas.negi10@gmail.com>
2023-11-18 15:48:17 +0100

    Duration callback is OK

--- nft_tracker/pages/floor_price/controller.py
+++ nft_tracker/pages/floor_price/controller.py
@@ -2,7 +2,10 @@ from pages.floor_price.view import app
 from dash.dependencies import Input, Output
 from pages.floor_price.model import plot_nft_hist
 
-@app.callback(Output(component_id = 'line_plot', component_property = 'figure'),
-              [Input(component_id = 'NFT_ID', component_property= 'value')])
-def graph_update(dropdown_value):
-    return plot_nft_hist(dropdown_value, 30)
+@app.callback(
+              Output(component_id = 'line_plot', component_property = 'figure'),
+              Input(component_id = 'NFT_ID', component_property= 'value'),
+              Input(component_id = 'DURATION', component_property= 'value')
+              )
+def graph_update(nft_id, duration):
+    return plot_nft_hist(nft_id, int(duration))
