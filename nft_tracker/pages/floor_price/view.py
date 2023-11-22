4c1b49a3beda66b1175074453fff00162c3ceef9
Vikas Negi <vikas.negi10@gmail.com>
2023-11-22 22:31:08 +0100

    Add labels to dashboard components

--- nft_tracker/pages/floor_price/view.py
+++ nft_tracker/pages/floor_price/view.py
@@ -13,17 +13,39 @@ layout = html.Div(id = 'parent',
                   style = {'textAlign':'center', 'marginTop':40, 'marginBottom':40}),
 
                   # Select mode
+                  html.Div([
+                  html.P('Select mode')
+                  ], style = {'marginBottom': 10, 'marginTop': 10}),
+
                   mode_dropdown(),
                   html.Br(), # adds space between the rows
 
                   # Options for NFT markets
+                  html.Div([
+                  html.P('Order by')
+                  ], style = {'marginBottom': 10, 'marginTop': 10}),
                   orderby_dropdown(),
+
                   html.Br(),
+
+                  html.Div([
+                  html.P('Number of entries')
+                  ], style = {'marginBottom': 10, 'marginTop': 10}),
                   entries_radiobutton(),
+
+                  html.Br(),
                   
                   # Options for NFT historical data
+                  html.Div([
+                  html.P('Select NFT ID')
+                  ], style = {'marginBottom': 10, 'marginTop': 10}),
                   nft_dropdown(),
+
                   html.Br(),
+
+                  html.Div([
+                  html.P('Historical data [days]')
+                  ], style = {'marginBottom': 10, 'marginTop': 10}),
                   duration_radiobutton(),
 
                   # Add plot
