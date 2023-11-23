1335671896e7715f7b50e62b98acebbb271ceca9
Vikas Negi <vikas.negi10@gmail.com>
2023-11-23 09:55:21 +0100

    Update margins

--- nft_tracker/pages/floor_price/view.py
+++ nft_tracker/pages/floor_price/view.py
@@ -10,12 +10,12 @@ app = dash.Dash()
 layout = html.Div(id = 'parent', 
                   children = [
                   html.H1(id = 'H1', children = 'NFT Floor Price Tracker', 
-                  style = {'textAlign':'center', 'marginTop':40, 'marginBottom':40}),
+                  style = {'textAlign':'center', 'marginTop': 10, 'marginBottom': 10}),
 
                   # Select mode
                   html.Div([
                   html.P('Select mode')
-                  ], style = {'marginBottom': 10, 'marginTop': 10}),
+                  ], style = {'marginBottom': 2, 'marginTop': 2}),
 
                   mode_dropdown(),
                   html.Br(), # adds space between the rows
@@ -23,14 +23,14 @@ layout = html.Div(id = 'parent',
                   # Options for NFT markets
                   html.Div([
                   html.P('Order by')
-                  ], style = {'marginBottom': 10, 'marginTop': 10}),
+                  ], style = {'marginBottom': 2, 'marginTop': 2}),
                   orderby_dropdown(),
 
                   html.Br(),
 
                   html.Div([
                   html.P('Number of entries')
-                  ], style = {'marginBottom': 10, 'marginTop': 10}),
+                  ], style = {'marginBottom': 2, 'marginTop': 2}),
                   entries_radiobutton(),
 
                   html.Br(),
@@ -38,21 +38,21 @@ layout = html.Div(id = 'parent',
                   # Options for NFT historical data
                   html.Div([
                   html.P('Select NFT ID')
-                  ], style = {'marginBottom': 10, 'marginTop': 10}),
+                  ], style = {'marginBottom': 2, 'marginTop': 2}),
                   nft_dropdown(),
 
                   html.Br(),
 
                   html.Div([
                   html.P('Historical data [days]')
-                  ], style = {'marginBottom': 10, 'marginTop': 10}),
+                  ], style = {'marginBottom': 2, 'marginTop': 2}),
                   duration_radiobutton(),
 
                   html.Br(),
 
                   html.Div([
                   html.P('Moving Average Window [days]')
-                  ], style = {'marginBottom': 10, 'marginTop': 10}),
+                  ], style = {'marginBottom': 2, 'marginTop': 2}),
                   window_radiobutton(),
 
                   # Add plot
